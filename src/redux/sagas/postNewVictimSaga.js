2cc70ca7851f00943c5a2ef4183f6a4e26bdd878
Sarah Bloomberg <sarah.l.bloomberg@gmail.com>
2018-05-18 14:52:55 -0500

    added postNewVictim saga and reducer

---
+++ src/redux/sagas/postNewVictimSaga.js
@@ -0,0 +1,23 @@
+import { call, put, takeEvery } from 'redux-saga/effects';
+import axios from 'axios';
+
+// enter a new victim contact form into the database and 
+// return new ID number which will be written on the paper form
+function* postNewVictim(action) {
+    try {
+       let createNewVictim = yield call(axios.post, '/api/victim', action.payload)
+       
+       yield put ({
+           type: 'DISPLAY_VICTIM_ID_NUMBER',
+           payload: createNewVictim.data.id
+       })
+    } catch (error) {
+        console.log('error in POST new victim', error)
+    }
+}
+
+function* postNewVictimSaga() {
+    yield takeEvery('ADD_NEW_VICTIM', postNewVictim)
+}
+
+export default postNewVictimSaga;
