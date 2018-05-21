57ded9ea98bb10047b007615a8f9375d46db8f24
Blake Burbach <cras.bburbach@gmail.com>
2018-05-21 12:26:06 -0500

    got YellowForm talking to redux

--- src/components/YellowForm/YellowForm.js
+++ src/components/YellowForm/YellowForm.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import InPersonContactInfo from '../FormComponents/ContactInfo/InPersonContactInfo';
 import InpersonServices from '../FormComponents/InPersonServices/InPersonServices';
 import UnmetNeeds from '../FormComponents/UnmetNeeds/UnmetNeeds';
 import Referrals from '../FormComponents/Referrals/Referrals';
 import Demographics from '../FormComponents/Demographics/Demographics';
 
+const mapStateToProps = state => ({
+    state
+})
 
 class YellowForm extends Component {
+
+    handleSubmit = () => {
+        this.props.dispatch({
+            type: 'ADD_NEW_VICTIM'
+        })
+    }
+
     render() {
         return (
             <div>
@@ -16,9 +27,10 @@ class YellowForm extends Component {
                 <UnmetNeeds />
                 <Referrals />
                 <Demographics />
+                <button onClick={this.handleSubmit}>Submit</button>
             </div>
         )
     }
 }
 
-export default YellowForm;
+export default connect(mapStateToProps)(YellowForm);
