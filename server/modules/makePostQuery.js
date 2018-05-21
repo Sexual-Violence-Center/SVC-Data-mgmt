2063cb2732e64249bad7decb27b91a142eaca775
Stephen Engler <sengler1989@gmail.com>
2018-05-21 11:20:44 -0500

    add makePostQuery function

---
+++ server/modules/makePostQuery.js
@@ -0,0 +1,32 @@
+const makeQuery = toInsert => {
+  //gets keys off of the object puts into an array
+  const keys = Object.keys(toInsert);
+  //getys all values and puts into array
+  const values = Object.values(toInsert);
+
+  //beginning of the query
+  let queryText = `INSERT INTO "victim" (`;
+  //takes each key which should correspond to a table column and puts into query
+  keys.forEach(key => {
+    queryText += `"${key}", `;
+  });
+
+  //takes of last comma
+  queryText = queryText.slice(0, -2);
+  //add closing parenthasis and adds values
+  queryText += ") VALUES (";
+
+  // for each value, makes a $value in query to sanatize the text
+  for (let i = 1; i <= values.length; i++) {
+    queryText += `$${i}, `;
+  }
+  // removes last comma
+  queryText = queryText.slice(0, -2);
+  //end of query
+  queryText += `) RETURNING *;`;
+  // returns the queryText and values as part of an object
+  //pool.query(queryText.queryText, queryText.values)
+  return { queryText: queryText, values: values };
+};
+
+module.exports = makeQuery;
