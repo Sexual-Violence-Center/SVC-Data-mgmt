32051788d0bc2e50a5a4b50aae3b823ee6c916e1
Ty Richadson <tyLrichardson63@gmail.com>
2018-05-21 16:05:47 -0500

    completed/tested in Postmand: server side DELETE of single victim record by logged-in user = admin user_type true

--- server/routes/victim.router.js
+++ server/routes/victim.router.js
@@ -38,14 +38,22 @@ router.put('/:id', (req, res)=>{
 })
 
 //Delete route for a victim form
-// TODO  change so only an admin can delete a form
-router.delete('/:id', (req, res)=>{
-    if(req.isAuthenticated()){
-        //pool.query goes here
-        res.sendStatus(200);
+// Only an admin can delete a form (in db person table, user_type is boolean; true = admin; false = user)
+router.delete('/:id', (req, res) => {
+    // console.log('authenticated user DELETE server route for Archive Page, req.params is:', req.params);
+    if(req.isAuthenticated() && req.user.user_type === true) {
+      let queryText = 'DELETE FROM "victim" WHERE id = $1;';
+      pool.query(queryText, [req.params.id])
+      .then((result) => {
+        console.log('DELETE successful', result);
+          res.sendStatus(200);
+      }).catch((error) => {
+        console.log('error in DELETE, server side', error);
+        res.sendStatus(500);
+      })
     } else {
-        res.sendStatus(403);
+      res.sendStatus(403);
     }
-})
+  });
 
 module.exports = router;
