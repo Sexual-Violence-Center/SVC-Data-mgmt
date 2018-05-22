607edeac9629aaa18e1414bf5722d8b765059ebb
Sarah Bloomberg <sarah.l.bloomberg@gmail.com>
2018-05-22 12:06:01 -0500

    second draft GET fed report server side

--- server/routes/report.federal.router.js
+++ server/routes/report.federal.router.js
@@ -2,16 +2,20 @@ const express = require("express");
 const pool = require("../modules/pool");
 const router = express.Router();
 
+const fedQueryText = require('./fed-report-query-text');
+
 // TODO make so only the admin can get the reports
 
 //Get for Federal Report
 router.get("/", (req, res) => {
   if (req.isAuthenticated()  && req.user.user_type === true) {
+    console.log('in GET federal reports', req.query.startDate, req.query.endDate);
+    
     // pool.query goes here
-      let queryText = `SELECT * FROM "victim" WHERE "contact_date" BETWEEN $1 AND $2;`
-      pool.query(queryText, [req.body.startDate, req.body.endDate])
+      let queryText = fedQueryText
+      pool.query(queryText, [req.query.startDate, req.query.endDate])
       .then((result) => {
-        console.log('GET fed reports success', result.rows);
+        console.log('GET fed reports success', result);
         res.send(result.rows)
       }).catch((error) => {
         console.log('error in GET fed reports', error);
