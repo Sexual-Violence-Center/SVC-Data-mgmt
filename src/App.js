94fa684a09042d1f924d8f773aa95d0fe3f1ed21
Ty Richadson <tyLrichardson63@gmail.com>
2018-05-22 15:02:11 -0500

    deleted AdminTools.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,9 @@ import FederalReport from './components/FederalReport/FederalReport';
 // import CountyReport from './components/CountyReport/CountyReport';
 // import CustomReport from './components/CustomReport/CustomReport';
 // import UserLandingPage from './components/UserLandingPage/UserLandingPage';
-// import AdminLandingPage from './components/AdminLandingPage/AdminLandingPage';
+import AdminLandingPage from './components/AdminLandingPage/AdminLandingPage';
+
+// import UserEntryPage from './components/AdminTools'
 
 
 import './styles/main.css';
@@ -72,10 +74,10 @@ const App = () => (
           path="/UserLandingPage"
           component={UserLandingPage}
         /> */}
-        {/* <Route
+        <Route
           path="/AdminLandingPage"
           component={AdminLandingPage}
-        /> */}
+        />
         {/* OTHERWISE (no path!) */}
         <Route render={() => <h1>404</h1>} />
 
