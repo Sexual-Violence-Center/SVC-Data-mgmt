f89c175e060ca44ecae30ea64c728660e6ba9e66
Ty Richadson <tyLrichardson63@gmail.com>
2018-05-22 14:27:42 -0500

    making UserEntryPage.js available to Blake

--- src/components/AdminLandingPage/AdminLandingPage.js
+++ src/components/AdminLandingPage/AdminLandingPage.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import UserEntryPage from '../AdminTools/UserEntryPage';
-
-const mapStateToProps = state => ({
-  state
-})
 
 class AdminLandingPage extends Component {
 
   render(){
     return (
       <div>
-        <h1>Admin Landing Page</h1>
+        <div>
+          <h1>Instructions</h1>
+        </div>
+        <div>
+          <h2>Welcome!</h2>
+          <p>
+            From here you can view the Federal or County reports by clicking the buttons on the left hand side of your screen. You also have access to the Data Entry View. The "Search/Update" button allows you to enter a confirmation number for a form that has already been entered so you can view the information that is in that specific form.
+          </p>
+        </div>
       </div>
     )
   }
 }
 
-export default connect(mapStateToProps)(AdminLandingPage);
+export default (AdminLandingPage);
