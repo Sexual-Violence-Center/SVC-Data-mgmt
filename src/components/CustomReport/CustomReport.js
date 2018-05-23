20ef5c9d30645e87fc0929584cc5031edd46eafa
Sarah Bloomberg <sarah.l.bloomberg@gmail.com>
2018-05-23 14:04:40 -0500

    working on adding topics

--- src/components/CustomReport/CustomReport.js
+++ src/components/CustomReport/CustomReport.js
@@ -20,10 +20,32 @@ class customReportSelectionPage extends Component{
 
     render(){
 
+        const customReportTopic = [
+            'Age', 'Contact Type', 'Disabilities', 
+            'Gender Identity', 'Immigrant Country', 
+            'Individuals Serviced', 'In-Person Crisis Counseling', 
+            'In-Person Legal Advocacy(Civil)', 'In-Person Legal Advocacy(Criminal)', 
+            'In-Person Medical Advocacy', 'Other In-Person Advocacy', 
+            'Phone Services Provided', 'Police Report Filed', 'Race/Ethnicity', 
+            'Referrals', 'Sexual Orientation', 
+            'Special Classification of Victims', 'Supported on Call', 
+            'Transgender', 'Transportation', 'Victim Types(Primary / Secondary)', 
+            'Victimization Types(Totals)', 'Un-Met Needs', 'Zip Codes'
+        ]
+
+        // let individualTopic = customReportTopic.map
+
+
         return ( 
             <div >
-                <Nav/>
                 <h2> Custom Report Page </h2>
+                <form action="/action_page.php">
+                    <select name="cars" multiple>
+                    {/* <option value={individualTopic}> {individualTopic} </option>
+                    {individualTopic} */}
+                    </select>
+                    <input type="submit" />
+                </form>
             </div>
 
         )// end return
