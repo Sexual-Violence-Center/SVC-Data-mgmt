fd1b8882502dad5e446a7b3c816724237a065781
Blake Burbach <cras.bburbach@gmail.com>
2018-05-24 18:52:19 -0500

    added info to admin's data entry page

--- src/components/DataEntryPage/DataEntryPage.js
+++ src/components/DataEntryPage/DataEntryPage.js
@@ -5,13 +5,26 @@ import AdminDataEntryNav from '../Nav/DataEntryNav/AdminDataEntryNav';
 
 class DataEntryPage extends Component {
     render(){
-        return(
+        return (
+          <div>
+              <AdminDataEntryNav />
             <div>
-                <AdminDataEntryNav />
-                <h1>Data Entry Page</h1>
+              <h1>Instructions</h1>
             </div>
+            <div>
+              <h2>Welcome!</h2>
+              <p>
+                SVC input forms are color coded to represent the type of service that was provided. Pink forms are for telephone services, and <br/>
+                yellow is for in-person services.
+                <br />
+                <br/>
+                The required data entry portal for your form can be found on the left hand side of your screen. Select the form that matches the type <br/>
+                of form you're entering.
+              </p>
+            </div>
+          </div>
         )
+      }
     }
-}
 
 export default DataEntryPage;
