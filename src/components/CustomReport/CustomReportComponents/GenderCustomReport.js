4ebd17d2f8027bb445de58658f28aa5c5a80cde7
Sarah Bloomberg <sarah.l.bloomberg@gmail.com>
2018-05-25 20:19:47 -0500

    disability and gender

--- src/components/CustomReport/CustomReportComponents/GenderCustomReport.js
+++ src/components/CustomReport/CustomReportComponents/GenderCustomReport.js
@@ -1,89 +1,143 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import PropTypes from 'prop-types';
+import keycode from 'keycode';
+import Downshift from 'downshift';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import MenuItem from '@material-ui/core/MenuItem';
+import Chip from '@material-ui/core/Chip';
 
-const mapStateToProps = state => ({
-    user: state.user,
-    state,
-});
+import GenderObject from '../ObjectLists/Gender.Object';
+import renderInput from '../StandardFunctionsForChips/renderInputFunction';
+import renderSuggestion from '../StandardFunctionsForChips/renderSuggestion'
+import styles from '../StandardFunctionsForChips/chipStyles'
 
-class CustomGender extends Component {
-    constructor() {
-        super();
-        this.state = {
-            contact_gendertype: '',
-        }
+
+function getSuggestions(inputValue) {
+  let count = 0;
+
+  return GenderObject.filter(suggestion => {
+    const keep =
+      (!inputValue || suggestion.label.toLowerCase().indexOf(inputValue.toLowerCase()) !== -1) &&
+      count < 5;
+    if (keep) {
+      count += 1;
+    }
+    return keep;
+  });
+}
+
+renderSuggestion.propTypes = {
+    highlightedIndex: PropTypes.number,
+    index: PropTypes.number,
+    itemProps: PropTypes.object,
+    selectedItem: PropTypes.string,
+    suggestion: PropTypes.shape({
+        label: PropTypes.string
+    }).isRequired,
+};
+
+class ContactTypeCustom extends React.Component {
+  state = {
+    inputValue: '',
+    selectedItem: [],
+  };
+
+  handleKeyDown = event => {
+    const { inputValue, selectedItem } = this.state;
+    if (selectedItem.length && !inputValue.length && keycode(event) === 'backspace') {
+      this.setState({
+        selectedItem: selectedItem.slice(0, selectedItem.length - 1),
+      });
+    }
+  };
+
+  handleInputChange = event => {
+    this.setState({ inputValue: event.target.value });
+  };
+
+  handleChange = item => {
+    let { selectedItem } = this.state;
+    if (selectedItem.indexOf(item) === -1) {
+      selectedItem = [...selectedItem, item];
     }
 
-     handleChangeFor = (event) => {
-        const target = event.target;
-        const value = target.type === ('checkbox') ? target.checked : target.value;
+    this.setState({
+      inputValue: '',
+      selectedItem,
+    });
+  };
 
-        this.setState({
-            contact_gendertype: value
-        });
-        
-     }
+  handleDelete = item => () => {
+    const selectedItem = [...this.state.selectedItem];
+    selectedItem.splice(selectedItem.indexOf(item), 1);
+    this.setState({ selectedItem });
+  };
 
-    render () {
+  render() {
+    const { classes } = this.props;
+    const { inputValue, selectedItem } = this.state;
+    console.log('selectedItem', selectedItem);
+    console.log('value', selectedItem);
+    
+    return (
+      <Downshift inputValue={inputValue} onChange={this.handleChange} selectedItem={selectedItem}>
+      
+        {({
+          getInputProps,
+          getItemProps,
+          isOpen,
+          inputValue: inputValue2,
+          selectedItem: selectedItem2,
+          highlightedIndex,
+        }) => (
+          <div className={classes.container}>
+          {/* <h4> Custom Contact Type Report </h4> */}
+            {renderInput({
+              fullWidth: true,
+              classes,
+              InputProps: getInputProps({
+                startAdornment: selectedItem.map(item => (
+                  <Chip
+                    key={item.value}
+                    tabIndex={-1}
+                    label={item.label}
+                    className={classes.chip}
+                    onDelete={this.handleDelete(item)}
+                    value={item.value}
+                  />
+                )),
+                onChange: this.handleInputChange,
+                onKeyDown: this.handleKeyDown,
+                placeholder: 'Select Gender Types',
+                id: 'integration-downshift-multiple',
+              }),
+            })}
+            {isOpen ? (
+              <Paper className={classes.paper} square>
+                {getSuggestions(inputValue2).map((suggestion, index) =>
+                  renderSuggestion({
+                    suggestion,
+                    index,
+                    itemProps: getItemProps({ item: suggestion }),
+                    highlightedIndex,
+                    selectedItem: selectedItem2,
+                  }),
+                )}
+              </Paper>
+            ) : null}
+          </div>
+        )}
+      </Downshift>
+    );
+  }
+}
 
-        return(
-            <div>
-                <h4> Custom Gender Report: </h4>
-                <form value={this.state.contact_gendertype} 
-                    onClick={this.handleChangeFor}
-                >
-                <input type="checkbox" 
-                    id = "victim_gender_male"
-                    value="victim_gender_male"
-                />
-                    <label htmlFor="victim_gender_male">
-                        Male                    
-                    </label>
+ContactTypeCustom.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
-                <input type="checkbox" 
-                    id = "victim_gender_female"
-                    value="victim_gender_female"
-                />
-                    <label htmlFor="victim_gender_female">
-                        Female
-                    </label>
+export default withStyles(styles)(ContactTypeCustom);
 
-                <input type="checkbox" 
-                    id = "victim_gender_non_binary"
-                    value="victim_gender_non_binary"
-                />
-                    <label htmlFor="victim_gender_non_binary">
-                        Non-Binary
-                    </label>
-
-                <input type="checkbox" 
-                    id = "victim_gender_other"
-                    value = "victim_gender_other"
-                />
-                    <label htmlFor = "victim_gender_other">
-                        Other
-                    </label>
-
-                <input type="checkbox" 
-                    id = "victim_gender_not_reported"
-                    value = "victim_gender_not_reported"
-                />
-                    <label htmlFor = "victim_gender_not_reported">
-                        Not Reported
-                    </label>
-
-                <input type="checkbox" 
-                    id = "total_gender_count"
-                    value="total_gender_count" 
-                />
-                    <label htmlFor="total_gender_count">
-                        Total
-                    </label>
-
-                </form>
-            </div>
-        )
-    } //end render
-}//end class
-
-export default connect(mapStateToProps)(CustomGender)
+    
