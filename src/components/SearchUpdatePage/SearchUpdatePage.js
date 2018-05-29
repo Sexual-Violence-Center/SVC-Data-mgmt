17ee34a53785dd77dca604010400d0dfe092f13c
Stephen Engler <sengler1989@gmail.com>
2018-05-29 14:14:36 -0500

    saga now updates the db

--- src/components/SearchUpdatePage/SearchUpdatePage.js
+++ src/components/SearchUpdatePage/SearchUpdatePage.js
@@ -30,7 +30,7 @@ class SearchUpdatePage extends Component {
     // if there is data in the reducer it means the user searched for something
     //shows form if true
     renderForm = () => {
-        if (this.props.state.updateFormReducer.id) {
+        if (this.props.state.updateFormReducer && this.props.state.updateFormReducer.id) {
             return <UpdateForm />
         }
     }
