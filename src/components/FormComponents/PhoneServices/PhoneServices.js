a5871ba01efc90d35673373bb731246dba6204db
Blake Burbach <cras.bburbach@gmail.com>
2018-05-30 18:07:38 -0500

    PhoneServices styled up

--- src/components/FormComponents/PhoneServices/PhoneServices.js
+++ src/components/FormComponents/PhoneServices/PhoneServices.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Card } from '@material-ui/core';
+import CrisisCounseling from './PhoneServicesComponents/CrisisCounseling';
+import InformationReferral from './PhoneServicesComponents/InformationReferral';
+import CriminalJustice from './PhoneServicesComponents/CriminalJustice';
+import OtherEmergency from './PhoneServicesComponents/OtherEmergency';
+import FinancialEmergency from './PhoneServicesComponents/FinancialEmergency';
+import ReparationsClaims from './PhoneServicesComponents/ReparationsClaims';
+
 
 const mapStateToProps = state => ({
     state
@@ -46,17 +53,12 @@ class PhoneServices extends Component {
                     <h2>Telephone Services</h2>
                     <div className="telephoneChoices">
                         <p>(check all that apply)</p>
-                        <input type="checkbox" id="crisis_counseling" name="crisis_counseling" value={this.state.crisis_counseling} onChange={this.handleChangeFor} /><label htmlFor="crisis_counseling">Crisis Counseling</label>
-                        <br />
-                        <input type="checkbox" id="information_referral" name="information_referral" value={this.state.information_referral} onChange={this.handleChangeFor} /><label htmlFor="information_referral">Info and/or referral(including appointments/intakes for service</label>
-                        <br />
-                        <input type="checkbox" id="information_criminal_justice" name="information_criminal_justice" value={this.state.information_criminal_justice} onChange={this.handleChangeFor} /><label htmlFor="information_criminal_justice">Information about the criminal justice process</label>
-                        <br />
-                        <input type="checkbox" id="other_emergency_justice" name="other_emergency_justice" value={this.state.other_emergency_justice} onChange={this.handleChangeFor} /><label htmlFor="other_emergency_justice">Other emergency justice-related assistance</label>
-                        <br />
-                        <input type="checkbox" id="emergency_financial" name="emergency_financial" value={this.state.emergency_financial} onChange={this.handleChangeFor} /><label htmlFor="emergency_financial">Emergency Financial Assistance</label>
-                        <br />
-                        <input type="checkbox" id="reparations_claims" name="reparations_claims" value={this.state.reparations_claims} onChange={this.handleChangeFor} /><label htmlFor="reparations_claims">Reparations Claims Assistance</label>
+                        <CrisisCounseling handleChangeFor={this.handleChangeFor}/>
+                        <InformationReferral handleChangeFor={this.handleChangeFor}/>
+                        <CriminalJustice handleChangeFor={this.handleChangeFor}/>
+                        <OtherEmergency handleChangeFor={this.handleChangeFor}/>
+                        <FinancialEmergency handleChangeFor={this.handleChangeFor}/>
+                        <ReparationsClaims handleChangeFor={this.handleChangeFor}/>
                     </div>
                 </Card>
             </div>
