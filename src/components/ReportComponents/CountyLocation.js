d5fe35a7bf08059be54f81c353549036ab767e9c
Sarah Bloomberg <sarah.l.bloomberg@gmail.com>
2018-05-31 11:48:55 -0500

    created county location.js

---
+++ src/components/ReportComponents/CountyLocation.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { USER_ACTIONS } from '../../redux/actions/userActions';
+import AdminNav from '../Nav/AdminNav/AdminNav';
+
+const mapStateToProps = state => ({
+    user: state.user,
+    county: state.getCountyReducer,
+    state
+});
+
+class CountyLocationType extends Component {
+    constructor() {
+        super();
+        this.state = {
+            startDate: '',
+            endDate: '',
+            county: '',
+            service_location: ''
+        }
+    }
+
+
+    handleChangeFor = (event) => {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+
+    }
+    submit = () => {
+        this.props.dispatch({
+            type: 'GET_PERSON_DATA_COUNTY',
+            payload: this.state
+        })
+    }
+    print = () => {
+        console.log('print button clicked');
+        window.print();
+    }
+
+    componentDidMount() {
+
+        this.props.dispatch({ type: USER_ACTIONS.FETCH_USER });
+        console.log(this.props.state.getCountyReducer)
+    }
+    render() {
+        return (
+            <section className="county">
+                <h4>New Hennepin County residents by victim type who recieved services during the reporting period</h4>
+                <table className="countyTable">
+                    <thead>
+                        <tr>
+                            <th>Type of Individual</th>
+                            <th>Number</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Adult Primary Victim/Survivor</td>
+                            <td>{this.props.state.getCountyReducer.amerIndAlaskaNative}</td>
+                        </tr>
+                        <tr>
+                            <td>Adult Secondary Victim/Survivor</td>
+                            <td>{this.props.state.getCountyReducer.victim_ethnicity_asian}</td>
+                        </tr>
+                        <tr>
+                            <td>Youth Primary Victim/Survivor</td>
+                            <td>{this.props.state.getCountyReducer.africanAmerican}</td>
+                        </tr>
+                        <tr>
+                            <td>Youth Secondary Victim/Survivor</td>
+                            <td>{this.props.state.getCountyReducer.hispanicOrLatino}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </section>
+        )
+    }
+}
+export default connect(mapStateToProps)(CountyLocationType);
