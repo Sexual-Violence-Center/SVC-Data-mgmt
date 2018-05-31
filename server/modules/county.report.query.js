688032ae8498b5de9bdd07d4c934f6282d89fa7e
Sarah Bloomberg <sarah.l.bloomberg@gmail.com>
2018-05-31 11:44:53 -0500

    added other location to query

--- server/modules/county.report.query.js
+++ server/modules/county.report.query.js
@@ -36,6 +36,26 @@ const queryText = `SELECT
    	(select COUNT(*) FROM "victim" WHERE "service_county" = $1 AND "service_location" = 'Shakopee' AND "contact_date" BETWEEN  $2 AND $3) as "location_Shakopee",
    	(select COUNT(*) FROM "victim" WHERE "service_county" = $1 AND "service_location" = 'Shakopee Correctional Facility' AND "contact_date" BETWEEN  $2 AND $3) as "location_Shakopee_Correctional_Facility",
    	(select COUNT(*) FROM "victim" WHERE "service_county" = $1 AND "service_location" = 'St. Francis Hospital' AND "contact_date" BETWEEN  $2 AND $3) as "location_St_Francis",
+   	(select COUNT(*) FROM "victim" WHERE "service_county" = $1 AND (
+		   "service_location" != '212 Medical Center' AND
+		   "service_location" != 'Abbott Northwestern Hospital' AND
+		   "service_location" != 'Belle Plaine High School' AND
+		   "service_location" != 'Fairview Southdale Hospital' AND
+		   "service_location" != 'Hennepin County Jail' AND
+		   "service_location" != 'HCMC' AND
+		   "service_location" != 'House of Charity' AND
+		   "service_location" != 'Maple Grove Hospital' AND
+		   "service_location" != 'Methodist Hospital' AND
+		   "service_location" != 'New Prague High School' AND
+		   "service_location" != 'PEASE' AND
+		   "service_location" != 'Plymouth' AND
+		   "service_location" != 'Richfield Health Services' AND
+		   "service_location" != 'Scott County Jail' AND
+		   "service_location" != 'Sexual Violence Center' AND
+		   "service_location" != 'Shakopee' AND
+		   "service_location" != 'Shakopee Correctional Facility' AND
+		   "service_location" != 'St. Francis Hospital')
+			AND "contact_date" BETWEEN  $2 AND $3) as "location_other",
 
     (select count(*) from "victim" WHERE "service_county" = $1 AND "victim_age" BETWEEN 0 AND 18 AND "contact_date" BETWEEN $2 AND $3) as victim_age_zero_to_eightteen,
     (select count(*) from "victim" WHERE "service_county" = $1 AND "victim_age" BETWEEN 19 AND 50 AND "contact_date" BETWEEN $2 AND $3) as victim_age_nineteen_to_fifty,
