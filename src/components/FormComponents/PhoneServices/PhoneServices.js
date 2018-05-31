f36fe144a3b3e57aa963338d3a74ce5c3d6a470a
Blake Burbach <cras.bburbach@gmail.com>
2018-05-30 20:31:51 -0500

    PhoneServices Grid up

--- src/components/FormComponents/PhoneServices/PhoneServices.js
+++ src/components/FormComponents/PhoneServices/PhoneServices.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Card } from '@material-ui/core';
+import { Card, Grid } from '@material-ui/core';
 import CrisisCounseling from './PhoneServicesComponents/CrisisCounseling';
 import InformationReferral from './PhoneServicesComponents/InformationReferral';
 import CriminalJustice from './PhoneServicesComponents/CriminalJustice';
@@ -50,15 +50,29 @@ class PhoneServices extends Component {
         return (
             <div className="phoneServices">
                 <Card style={style}>
-                    <h2>Telephone Services</h2>
-                    <div className="telephoneChoices">
+                    <div>
+                        <h2>Telephone Services</h2>
                         <p>(check all that apply)</p>
-                        <CrisisCounseling handleChangeFor={this.handleChangeFor}/>
-                        <InformationReferral handleChangeFor={this.handleChangeFor}/>
-                        <CriminalJustice handleChangeFor={this.handleChangeFor}/>
-                        <OtherEmergency handleChangeFor={this.handleChangeFor}/>
-                        <FinancialEmergency handleChangeFor={this.handleChangeFor}/>
-                        <ReparationsClaims handleChangeFor={this.handleChangeFor}/>
+                        <Grid container direction="column" justify="center" alignItems="flex-start">
+                            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                                <CrisisCounseling handleChangeFor={this.handleChangeFor} />
+                            </Grid>
+                            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                                <InformationReferral handleChangeFor={this.handleChangeFor} />
+                            </Grid>
+                            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                                <CriminalJustice handleChangeFor={this.handleChangeFor} />
+                            </Grid>
+                            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                                <OtherEmergency handleChangeFor={this.handleChangeFor} />
+                            </Grid>
+                            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                                <FinancialEmergency handleChangeFor={this.handleChangeFor} />
+                            </Grid>
+                            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                                <ReparationsClaims handleChangeFor={this.handleChangeFor} />
+                            </Grid>
+                        </Grid>
                     </div>
                 </Card>
             </div>
