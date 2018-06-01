799ff854522e7f7d09eaac89ff487938671c6859
Stephen Engler <sengler1989@gmail.com>
2018-06-01 12:35:56 -0500

    form clears up to survey

--- src/components/FormComponents/Demographics/DemographicComponents/PoliceReport.js
+++ src/components/FormComponents/Demographics/DemographicComponents/PoliceReport.js
@@ -16,52 +16,78 @@ const styles = theme => ({
     },
 });
 
+const initialState = {
+  police_report_filed: ""
+};
+
 class PoliceReport extends Component {
-    constructor(){
-        super();
-        this.state = {
-            police_report_filed: ''
+  constructor() {
+    super();
+    this.state = initialState;
+  }
+
+  static getDerivedStateFromProps = (props, state) => {
+    if (props.dispatchTo === "UPDATE_THE_FORM") {
+      const { updateFormReducer } = props.state;
+      Object.keys(updateFormReducer).forEach(key => {
+        if (updateFormReducer[key] === null) {
+          updateFormReducer[key] = undefined;
         }
+        return null;
+      });
+      return updateFormReducer;
+    } else if (!props.state.EntryFormReducer) {
+      return initialState;
+    } else {
+      return state;
     }
+  };
 
-    handleChangeFor = event => {
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
-    
-        this.setState({
-          [name]: value
-        });
-        this.props.dispatch({
-          type: this.props.dispatchTo,
-          payload: { ...this.state, [name]: value }
-        });
-    }
+  handleChangeFor = event => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
 
-    render() {
-        const { classes } = this.props;
-        return (
-            <div>
-                <h3>Was a police report filed?</h3>
-                <FormControl className={classes.formControl}>
-                    <InputLabel htmlFor="police_report_filed">Select One</InputLabel>
-                    <Select
-                        value={this.state.police_report_filed}
-                        onChange={this.handleChangeFor}
-                        inputProps={{
-                            name: 'police_report_filed',
-                            id: 'police_report_filed',
-                        }}>
-                        <MenuItem style={{width: '100%'}} value="yes">Yes</MenuItem>
-                        <Divider />
-                        <MenuItem style={{width: '100%'}} value="no">No</MenuItem>
-                        <Divider />
-                        <MenuItem style={{width: '100%'}} value="unknown">Unknown</MenuItem>
-                    </Select>
-                </FormControl>
-            </div>
-        )
-    }
+    this.setState({
+      [name]: value
+    });
+    this.props.dispatch({
+      type: this.props.dispatchTo,
+      payload: { ...this.state, [name]: value }
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <h3>Was a police report filed?</h3>
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="police_report_filed">Select One</InputLabel>
+          <Select
+            value={this.state.police_report_filed}
+            onChange={this.handleChangeFor}
+            inputProps={{
+              name: "police_report_filed",
+              id: "police_report_filed"
+            }}
+          >
+            <MenuItem style={{ width: "100%" }} value="yes">
+              Yes
+            </MenuItem>
+            <Divider />
+            <MenuItem style={{ width: "100%" }} value="no">
+              No
+            </MenuItem>
+            <Divider />
+            <MenuItem style={{ width: "100%" }} value="unknown">
+              Unknown
+            </MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+    );
+  }
 }
 
 PoliceReport.propTypes = {
