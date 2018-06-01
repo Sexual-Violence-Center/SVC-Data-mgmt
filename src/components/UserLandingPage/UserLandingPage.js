5c14553dc5c81d543590232f328c8ae4452db760
Sarah Bloomberg <sarah.l.bloomberg@gmail.com>
2018-06-01 09:33:22 -0500

    added new text for admin landing page

--- src/components/UserLandingPage/UserLandingPage.js
+++ src/components/UserLandingPage/UserLandingPage.js
@@ -44,8 +44,9 @@ class UserLandingPage extends Component {
                   <Divider />
                   <br />
                   <Typography variant="subheading">
-                    SVC input forms are color coded to represent the type of service that was provided. Pink forms are for telephone services, and <br />
-                    yellow is for in-person services.
+                    SVC input forms are color coded to represent the type of service that was provided. 
+                    <br /> - Pink forms are for telephone contacts.
+                     <br /> - Yellow form are for in-person contacts.
                   <br />
                     <br />
                     The required data entry portal for your form can be found on the left hand side of your screen. Select the form that matches the type <br />
