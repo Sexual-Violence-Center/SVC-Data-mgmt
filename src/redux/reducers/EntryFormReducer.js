a3a63cd27b566e913d0717f02878202bd006452a
Stephen Engler <sengler1989@gmail.com>
2018-06-07 11:36:45 -0500

    yellow form demographic only shows if haven't contacted before

--- src/redux/reducers/EntryFormReducer.js
+++ src/redux/reducers/EntryFormReducer.js
@@ -1,4 +1,9 @@
-const EntryFormReducer = (state = {}, action) => {
+const intialState = {
+  victim_prior_contact: "",
+  victim_contact_prior_oct: "",
+};
+
+const EntryFormReducer = (state = intialState, action) => {
     switch(action.type){
         case 'ENTRY_FORM_DATA':
             return {...state, ...action.payload};
