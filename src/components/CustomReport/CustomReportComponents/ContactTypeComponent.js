1819a37a65bc973ce7f87a6174624543c1b92115
Sarah Bloomberg <sarah.l.bloomberg@gmail.com>
2018-06-06 21:00:19 -0500

    mapped chips to selected item

--- src/components/CustomReport/CustomReportComponents/ContactTypeComponent.js
+++ src/components/CustomReport/CustomReportComponents/ContactTypeComponent.js
@@ -18,7 +18,6 @@ const mapStateToProps = state => ({
 
 function getSuggestions(inputValue) {
   let count = 0;
-
   return contactType.filter(suggestion => {
     const keep =
       (!inputValue || suggestion.label.toLowerCase().indexOf(inputValue.toLowerCase()) !== -1) &&
@@ -31,35 +30,19 @@ function getSuggestions(inputValue) {
 }
 
 renderSuggestion.propTypes = {
-    highlightedIndex: PropTypes.number,
-    index: PropTypes.number,
-    itemProps: PropTypes.object,
-    selectedItem: PropTypes.string,
-    suggestion: PropTypes.shape({
-        label: PropTypes.string
-    }).isRequired,
+  highlightedIndex: PropTypes.number,
+  index: PropTypes.number,
+  itemProps: PropTypes.object,
+  selectedItem: PropTypes.string,
+  suggestion: PropTypes.shape({
+    label: PropTypes.string
+  }).isRequired,
 };
 
 class ContactTypeCustom extends React.Component {
   state = {
     inputValue: '',
-    selectedItem: [],
-  };
-
-  handleChangeForComponent = (item) => {
-    let { selectedItem } = this.state;
-    if (selectedItem.indexOf(item) === -1) {
-      selectedItem = [...selectedItem, item];
-    }
-    this.setState({
-      inputValue: '',
-      selectedItem,
-    })
-    this.props.dispatch({
-      type: 'UPDATE_SELECTED_ITEM',
-      payload: { ...this.state, selectedItem}
-    })
-
+    // selectedItem: [],
   };
 
   handleInputChange = event => {
@@ -68,20 +51,22 @@ class ContactTypeCustom extends React.Component {
 
   handleKeyDown = event => {
     const { inputValue, selectedItem } = this.state;
-    if (selectedItem.length && !inputValue.length && keycode(event) === 'backspace') {
+    if (this.props.selectedItem.length && !this.props.inputValue.length && keycode(event) === 'backspace') {
       this.setState({
-        selectedItem: selectedItem.slice(0, selectedItem.length - 1),
+        selectedItem: this.props.selectedItem.slice(0, this.props.selectedItem.length - 1),
       });
     }
   };
 
   render() {
     const { classes } = this.props;
-    const { inputValue, selectedItem } = this.state;
+    const { inputValue } = this.state;
+    const { selectedItem } = this.props;
     
     return (
-      <Downshift inputValue={inputValue} onChange={this.handleChangeForComponent} selectedItem={selectedItem}>
-      
+      <Downshift inputValue={inputValue}
+        onChange={this.props.handleChangeForComponent}
+        selectedItem={selectedItem}>
         {({
           getInputProps,
           getItemProps,
@@ -90,43 +75,42 @@ class ContactTypeCustom extends React.Component {
           selectedItem: selectedItem2,
           highlightedIndex,
         }) => (
-          <div className={classes.container}>
-          {/* <h4> Custom Contact Type Report </h4> */}
-            {renderInput({
-              fullWidth: true,
-              classes,
-              InputProps: getInputProps({
-                startAdornment: selectedItem.map(item => (
-                  <Chip
-                    key={item.value}
-                    tabIndex={-1}
-                    label={item.label}
-                    className={classes.chip}
-                    onDelete={this.props.handleDelete(item)}
-                    value={item.value}
-                  />
-                )),
-                onChange: this.handleInputChange,
-                onKeyDown: this.handleKeyDown,
-                placeholder: 'Contact Types',
-                id: 'integration-downshift-multiple',
-              }),
-            })}
-            {isOpen ? (
-              <Paper className={classes.paper} square>
-                {getSuggestions(inputValue2).map((suggestion, index) =>
-                  renderSuggestion({
-                    suggestion,
-                    index,
-                    itemProps: getItemProps({ item: suggestion }),
-                    highlightedIndex,
-                    selectedItem: selectedItem2,
-                  }),
-                )}
-              </Paper>
-            ) : null}
-          </div>
-        )}
+            <div className={classes.container}>
+              {renderInput({
+                fullWidth: true,
+                classes,
+                InputProps: getInputProps({
+                  startAdornment: selectedItem.map(item => (
+                    <Chip
+                      key={item.value}
+                      tabIndex={-1}
+                      label={item.label}
+                      className={classes.chip}
+                      onDelete={this.props.handleDelete(item)}
+                      value={item.value}
+                    />
+                  )),
+                  onChange: this.handleInputChange,
+                  onKeyDown: this.handleKeyDown,
+                  placeholder: 'Contact Types',
+                  id: 'integration-downshift-multiple',
+                }),
+              })}
+              {isOpen ? (
+                <Paper className={classes.paper} square>
+                  {getSuggestions(inputValue2).map((suggestion, index) =>
+                    renderSuggestion({
+                      suggestion,
+                      index,
+                      itemProps: getItemProps({ item: suggestion }),
+                      highlightedIndex,
+                      selectedItem: selectedItem2,
+                    }),
+                  )}
+                </Paper>
+              ) : null}
+            </div>
+          )}
       </Downshift>
     );
   }
