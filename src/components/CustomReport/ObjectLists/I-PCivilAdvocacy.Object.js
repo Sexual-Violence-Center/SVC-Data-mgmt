db91687108df933623cfbbcad34c4a0dbb78f181
Blake Burbach <cras.bburbach@gmail.com>
2018-06-28 13:51:33 -0500

    legal_oft_hro

--- src/components/CustomReport/ObjectLists/I-PCivilAdvocacy.Object.js
+++ src/components/CustomReport/ObjectLists/I-PCivilAdvocacy.Object.js
@@ -1,5 +1,5 @@
 const CivilAdvocacyObject  = [
-    { label: 'Assistance Obtaining OFP or HRO', value: "ofp_hro" },
+    { label: 'Assistance Obtaining OFP or HRO', value: "legal_oft_hro" },
     { label: 'Immigration - Related Support', value: "legal_immigration" },
     { label: 'Intervention with Landlord, Academic Institution or Creditor', value: "legal_intervention" }
 ]
